ca1fbe811d5bcc527caf80e716b8c44323af4802
Muhammad-Zacky <zackym038@gmail.com>
2026-05-07 15:44:49 +0700

    Add Location model with mass assignment fields and factory support

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'latitude',
+        'longitude',
+        'type',
+    ];
 }
