2d6e9aca8601a779d47983dffe4eeceb403fd2f5
Muhammad-Zacky <zackym038@gmail.com>
2026-05-07 14:26:21 +0700

    Create migration for locations table with necessary fields

---
+++ database/migrations/2026_05_07_071513_create_locations_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nama Panti Asuhan / Posko
+            $table->text('address')->nullable(); // Alamat lengkap
+
+            // Decimal sangat cocok untuk menyimpan titik koordinat Latitude & Longitude Mapbox
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+
+            $table->string('type')->default('panti_asuhan'); // Contoh: panti_asuhan, posko_bencana, dll
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
